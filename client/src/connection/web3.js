bdde80d55b47f6299fd02e184286a888d406ec41
adimpdl <ghag@mpdl.mpg.de>
2021-08-31 11:50:25 +0200

    web3_chanegs

--- client/src/connection/web3.js
+++ client/src/connection/web3.js
@@ -1,18 +1,24 @@
-import Web3 from 'web3';
+import Web3 from 'web3'
 
 const getWeb3 = () => {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
   // window.addEventListener('load', async () => {
   // Modern dapp browsers...
   if (window.ethereum) {
-    const web3 = new Web3(window.ethereum);
-    return web3;
+    const web3 = new Web3(window.ethereum)
+    return web3
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    // Use Mist/MetaMask's provider.
+    const web3 = window.web3
+    return web3
   }
   // Fallback to localhost; use dev console port by default...
   else {
-    throw new Error('No web3 found');
+    throw new Error('No web3 found')
   }
   // });
-};
+}
 
-export default getWeb3; 
+export default getWeb3
